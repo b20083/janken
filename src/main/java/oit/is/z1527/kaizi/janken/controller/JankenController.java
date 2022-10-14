00e53edabf7d09940ea0c10da01feb7e29c9892b
b20083 <e1b20083@st.oit.ac.jp>
2022-10-14 14:31:28 +0900

    DIを用いた共有の実装

--- src/main/java/oit/is/z1527/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1527/kaizi/janken/controller/JankenController.java
@@ -1,11 +1,14 @@
 package oit.is.z1527.kaizi.janken.controller;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import oit.is.z1527.kaizi.janken.model.Entry;
 
 /**
  * Sample21Controller
@@ -15,8 +18,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class JankenController {
 
+  @Autowired
+  private Entry entry;
+
   @GetMapping("/janken")
-  public String janken() {
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
+    model.addAttribute("name", loginUser);
+    return "janken.html";
+  }
+
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
+
     return "janken.html";
   }
 
