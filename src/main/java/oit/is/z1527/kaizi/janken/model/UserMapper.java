92d8ce4c447f5191e338afeccfe19572b7f15cd2
b20083 <e1b20083@st.oit.ac.jp>
2022-10-28 10:21:45 +0900

    ユーザ情報の取得

---
+++ src/main/java/oit/is/z1527/kaizi/janken/model/UserMapper.java
@@ -0,0 +1,16 @@
+package oit.is.z1527.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+  @Select("SELECT id, name from users")
+  ArrayList<User> selectAllUser();
+
+}
